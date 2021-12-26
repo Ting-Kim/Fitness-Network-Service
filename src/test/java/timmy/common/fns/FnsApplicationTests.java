22f618e88504e0e086c22320f62d5cfa89bf0c7e
Ting-Kim <taeho916@gmail.com>
2021-12-26 23:23:15 +0900

    first commit

---
+++ src/test/java/timmy/common/fns/FnsApplicationTests.java
@@ -0,0 +1,13 @@
+package timmy.common.fns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
